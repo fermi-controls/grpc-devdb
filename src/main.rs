d4365569d047531e461543ad0d1bf5ee5ac19be7
Rich Neswold <neswold@fnal.gov>
2023-06-26 11:28:19 -0500

    :sparkles: provide digital control information

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use futures::{Stream, StreamExt};
 use proto::{
     dev_db_server::{DevDb, DevDbServer},
-    info_entry, DeviceInfo, DeviceInfoReply, DeviceList, InfoEntry, Property,
+    info_entry, DeviceInfo, DeviceInfoReply, DeviceList, DigitalControl, DigitalControlItem,
+    InfoEntry, Property,
 };
 use sqlx::postgres::{PgPool, PgPoolOptions};
 use std::pin::Pin;
@@ -49,6 +50,106 @@ SELECT di,
     JOIN accdb.property P USING(di)
     JOIN accdb.device_scaling S USING(di, pi)
   WHERE D.name = $1 and pi in (12, 13)"#;
+
+    const BC_QUERY: &str = r#"
+SELECT P.value as val,
+       CAST(P.short_name AS TEXT) as sname,
+       CAST(P.long_name AS TEXT) as lname
+  FROM accdb.device D
+    JOIN accdb.digital_control P USING(di)
+  WHERE D.name = $1
+  ORDER BY order_number"#;
+
+    async fn query_device(&self, item: &str) -> InfoEntry {
+        // Build and prep the SQL query for this iteration.
+
+        let mut sql_cmd: Fetch<RowInfo> = sqlx::query_as(DevDB::QUERY).bind(item).fetch(&self.pool);
+
+        // Local copies of the device info that we're accumulating.
+
+        let mut index: u32 = 0;
+        let mut description: String = "".into();
+        let mut r_prop: Option<Property> = None;
+        let mut s_prop: Option<Property> = None;
+
+        // Loop through the database results.
+
+        while let Some(row) = sql_cmd.next().await {
+            match row {
+                Ok(row) => {
+                    index = row.di as u32;
+                    description = row.descr.clone();
+
+                    // Build a property type.
+
+                    let prop = Property {
+                        primary_units: Some(row.p_units.clone()),
+                        common_units: Some(row.c_units.clone()),
+                    };
+
+                    // Now fill in the appropriate property. 12 is
+                    // for readings and 13 is for settings. Our
+                    // query only returns these two properties.
+
+                    if row.pi == 12 {
+                        r_prop = Some(prop)
+                    } else {
+                        s_prop = Some(prop)
+                    }
+                }
+                Err(e) => {
+                    warn!("couldn't decode row for {} : {}", &item, &e);
+                    let tmp = InfoEntry {
+                        name: item.into(),
+                        result: Some(info_entry::Result::ErrMsg(format!("{}", e))),
+                    };
+
+                    return tmp;
+                }
+            }
+        }
+
+        // Now look for digital control information.
+
+        let mut cmds = vec![];
+
+        {
+            let mut sql_cmd: Fetch<(i32, String, String)> =
+                sqlx::query_as(DevDB::BC_QUERY).bind(item).fetch(&self.pool);
+
+            while let Some(row) = sql_cmd.next().await {
+                match row {
+                    Ok((value, short_name, long_name)) => cmds.push(DigitalControlItem {
+                        value: value as u32,
+                        short_name,
+                        long_name,
+                    }),
+                    Err(e) => {
+                        error!("couldn't parse digital status : {}", e);
+                        cmds.clear();
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Build final value.
+
+        InfoEntry {
+            name: item.into(),
+            result: Some(info_entry::Result::Device(DeviceInfo {
+                index,
+                description,
+                reading: r_prop,
+                setting: s_prop,
+                dig_control: if cmds.is_empty() {
+                    None
+                } else {
+                    Some(DigitalControl { cmds })
+                },
+            })),
+        }
+    }
 }
 
 type Fetch<'a, T> = Pin<Box<dyn Stream<Item = Result<T, sqlx::Error>> + Send + 'a>>;
@@ -71,69 +172,8 @@ impl DevDb for DevDB {
 
             // Loop through each device.
 
-            'outer: for item in &request.get_ref().device {
-                // Build and prep the SQL query for this iteration.
-
-                let mut sql_cmd: Fetch<RowInfo> =
-                    sqlx::query_as(DevDB::QUERY).bind(item).fetch(&self.pool);
-
-                // Local copies of the device info that we're accumulating.
-
-                let mut index: u32 = 0;
-                let mut description: String = "".into();
-                let mut r_prop: Option<Property> = None;
-                let mut s_prop: Option<Property> = None;
-
-                // Loop through the database results.
-
-                while let Some(row) = sql_cmd.next().await {
-                    match row {
-                        Ok(row) => {
-                            index = row.di as u32;
-                            description = row.descr.clone();
-
-                            // Build a property type.
-
-                            let prop = Property {
-                                primary_units: Some(row.p_units.clone()),
-                                common_units: Some(row.c_units.clone()),
-                            };
-
-                            // Now fill in the appropriate property. 12 is
-                            // for readings and 13 is for settings. Our
-                            // query only returns these two properties.
-
-                            if row.pi == 12 {
-                                r_prop = Some(prop)
-                            } else {
-                                s_prop = Some(prop)
-                            }
-                        }
-                        Err(e) => {
-                            warn!("couldn't decode row for {} : {}", &item, &e);
-                            let tmp = InfoEntry {
-                                name: item.into(),
-                                result: Some(info_entry::Result::ErrMsg(format!("{}", e))),
-                            };
-
-                            result.push(tmp);
-                            break 'outer;
-                        }
-                    }
-                }
-
-                let tmp = InfoEntry {
-                    name: item.into(),
-                    result: Some(info_entry::Result::Device(DeviceInfo {
-                        index,
-                        description,
-                        reading: r_prop,
-                        setting: s_prop,
-                        dig_control: None,
-                    })),
-                };
-
-                result.push(tmp);
+            for item in &request.get_ref().device {
+                result.push(self.query_device(item).await)
             }
 
             Ok(Response::new(DeviceInfoReply { set: result }))
